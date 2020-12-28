ccd395ebe0a16c792cbe7ab5fefc0427a88a73b1
sho <66288825+sho12333@users.noreply.github.com>
2020-12-28 23:32:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ function onFileSelected(input) {
 
     var element = document.getElementById('file_name');
     element.innerHTML = file.name;
-    //console.log(element);
-
 
     var reader = new FileReader();
-
     reader.onload = onFileLoaded;
-
     reader.readAsDataURL(file);
 
 }
@@ -40,21 +36,43 @@ function onImageSetted(e) {
 
 }
 
+CANVAS_SIZE=256;
+
 //画像をキャンバスに配置する
 function createImageData(img) {
 
     var cv = document.createElement('canvas');
+    var canvas = document.getElementById('test_canvas')
 
     cv.width = img.naturalWidth;
     cv.height = img.naturalHeight;
 
     var ct = cv.getContext('2d');
 
-    ct.drawImage(img, 0, 0);
+    // 画像の圧縮
+    var canvas_width = canvas.width;
+    var canvas_height = canvas.height;
+    
+    var canvas_ratio = canvas_height / canvas_width;
+    var img_ratio = cv.height / cv.width;
+    if (img_ratio <=  canvas_ratio) {   // 横長のとき
+        var new_width = canvas_width;
+        var ratio_comp = canvas_width / cv.width;
+        var new_height = ratio_comp * cv.height;
+    }
+    else {               // 縦長のとき
+        new_height = canvas_height;
+        ratio_comp = canvas_height / cv.height;
+        new_width = ratio_comp * cv.width;
+
+    }
+
+    ct.drawImage(img, 0, 0, new_height, new_width);
 
     var data = ct.getImageData(0, 0, cv.width, cv.height);
 
     return data;
 
 }
+
 

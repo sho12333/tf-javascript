31053ad98cb21c123068b06a8898448c7bf8db3d
sho <miomini3715@gmail.com>
2023-04-22 17:50:01 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,88 +1,35 @@
-//ファイル選択
-function onFileSelected(input) {
-  let file = input.files[0];
+const inputElement = document.getElementById("file");
 
-  let element = document.getElementById("file_name");
-  element.innerHTML = file.name;
+inputElement.addEventListener("change", () => {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const img = new Image();
+    img.onload = (e) => setImage(e.target);
+    img.src = e.target.result;
+  };
 
-  let reader = new FileReader();
-  reader.onload = onFileLoaded;
-  reader.readAsDataURL(file);
-}
-
-//ファイル読み込み
-function onFileLoaded(e) {
-  let src_data = e.target.result;
-
-  let img = new Image();
-  img.onload = onImageSetted;
-  img.src = src_data;
-}
+  reader.readAsDataURL(inputElement.files[0]);
+});
 
 function updatePredictions() {
-  console.log("click");
-  const canvas = document.getElementById("test_canvas").getContext("2d");
+  const canvas = document.getElementById("canvas").getContext("2d");
   const imgData = canvas.getImageData(0, 0, 256, 256);
 
   cocoSsd.load().then((model) => {
     model.detect(imgData).then((predictions) => {
-      var element = document.getElementById("label_name");
-      element.innerHTML = predictions[0].class;
+      const lbl_element = document.getElementById("label");
+      lbl_element.innerHTML = predictions[0].class;
 
-      var element = document.getElementById("percent");
-      element.innerHTML = Math.round(predictions[0].score * 100) + "%";
+      const percent_element = document.getElementById("percent");
+      percent_element.innerHTML = Math.round(predictions[0].score * 100) + "%";
     });
   });
 }
 
-//画像をセットする
-function onImageSetted(e) {
-  let data = createImageData(e.target);
+function setImage(img) {
+  const ct = document.getElementById("canvas").getContext("2d");
+  ct.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-  document
-    .getElementById("test_canvas")
-    .getContext("2d")
-    .putImageData(data, 0, 0);
-}
-
-const CANVAS_SIZE = 256;
-
-//画像をキャンバスに配置する
-function createImageData(img) {
-  let cv = document.createElement("canvas");
-  let canvas = document.getElementById("test_canvas");
-
-  cv.width = img.naturalWidth;
-  cv.height = img.naturalHeight;
-
-  let ct = cv.getContext("2d");
-
-  // 画像の圧縮
-  let canvas_width = canvas.width;
-  let canvas_height = canvas.height;
-
-  let canvas_ratio = canvas_height / canvas_width;
-  let img_ratio = cv.height / cv.width;
-
-  let new_width;
-  let new_height;
-  let ratio_comp;
-
-  if (img_ratio <= canvas_ratio) {
-    // 横長のとき
-    new_width = canvas_width;
-    ratio_comp = canvas_width / cv.width;
-    new_height = ratio_comp * cv.height;
-  } else {
-    // 縦長のとき
-    new_height = canvas_height;
-    ratio_comp = canvas_height / cv.height;
-    new_width = ratio_comp * cv.width;
-  }
-
-  ct.drawImage(img, 0, 0, new_height, new_width);
-
-  let data = ct.getImageData(0, 0, cv.width, cv.height);
-
-  return data;
+  const data = ct.getImageData(0, 0, canvas.width, canvas.height);
+  document.getElementById("canvas").getContext("2d").putImageData(data, 0, 0);
 }
